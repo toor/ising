fdd529ebbf1b081ad15386af50afbc1543ff6081
toor <william@huddie.net>
2022-02-04 08:46:31 +0000

    Streamline calculation of average energy and spin

--- t2im.py
+++ t2im.py
@@ -2,7 +2,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-import matplotlib.colors
+import matplotlib.colors as colors
 # used for runtime stats
 import time
 from numba import jit
@@ -44,7 +44,7 @@ def nearest_neighbours(lattice, i, j):
 # Calculate the total energy by iterating over all lattice sites (spins) and calculating their contribution
 # to the exchange energy. The exchange interaction is assumed to be limited to nearest neighbours.
 @jit(nopython=True)
-def calculate_total_energy(lattice, J):
+def calculate_total_energy(lattice):
     energy = 0
     # rows, cols
     (x, y) = lattice.shape
@@ -55,7 +55,7 @@ def calculate_total_energy(lattice, J):
             spin = lattice[i][j]
             nn = nearest_neighbours(lattice, i, j)
             for neighbour in nn:
-                contribution = -J*spin*neighbour
+                contribution = -spin*neighbour
                 energy += contribution
    
     return energy
@@ -71,85 +71,107 @@ def calculate_total_spin(lattice):
             _spin = lattice[i][j]
             spin += _spin
     return spin
+@jit(nopython=True)
+def energy_and_spin(lattice, n):
+    E_total = calculate_total_energy(lattice)
+    E_2_total = np.power(E_total, 2)
+    E = np.divide(E_total, n)
+    E_2 = np.divide(E_2_total, n)
+
+    S_total = calculate_total_spin(lattice)
+    S_2_total = np.power(S_total, 2)
+    S = np.divide(S_total, n)
+    S_2 = np.divide(S_2_total, n)   
+
+    return E, E_2, S, S_2, E_total
 
 
 # Run metropolis on a specific lattice for a given temperature
 def metropolis(lattice, iterations, T):
-    (x, y) = lattice.shape
+    (x,y) = lattice.shape
     n = x*y
-    energies = np.zeros((iterations,))
-    spins = np.zeros((iterations,))
-    
+    energy = 0
+    # squared energy; will be used to plot the heat capacity
+    enrg_sq = 0
+    magnetisation = 0
+    mag_sq = 0
+
+
     # Choose an interaction energy that is of the same order as the thermal energy kT ~10^-21 for
     # T between 0 and 1000K
-    J = 1.0*np.power(10.0, -21)
-    t = 0
-    (x,y) = lattice.shape
+    J = np.power(10.0, -21)
     
+    t = 0  
     # Run a specified number of Monte Carlo steps
     while t < iterations:
         # return i in the interval [0, x - 1]
         i = np.random.randint(x)
         j = np.random.randint(y)
-        
+       
+        # calculate the total energy in units of J, and thus
+        # the average energy per spin 
         old = lattice[i][j]
-        E_i_total = calculate_total_energy(lattice, J)
-        E_i = np.divide(E_i_total, n)
-        initial_spin = calculate_total_spin(lattice)
-        
-        # update to the new state and calculate the new energy and total spin
+        E_i, E_2i, S_i, S_2i, E_i_total = energy_and_spin(lattice, n)
+
+        # update to the new state
         new = -1*old
         lattice[i][j] = new
-        E_f_total = calculate_total_energy(lattice, J)
-        E_f = np.divide(E_f_total, n)
-        final_spin = calculate_total_spin(lattice)
-        
+    
+        E_f, E_2f, S_f, S_2f, E_f_total = energy_and_spin(lattice, n)
+
         # determine the change in energy
-        delta_E = E_f - E_i
+        delta_E = E_f_total - E_i_total
         
         P = np.random.uniform()
         # boltzmann factor exp(-\delta E/k_B*T)
-        bz = np.exp(-delta_E/(k_B*T))
+        bz = np.exp(-delta_E*J/(k_B*T))
         
         # determine whether to accept the new state
-        if E_f < E_i:
+        if delta_E < 0:
+            energy += E_f
+            magnetisation += S_f
+            enrg_sq += E_2f
+            mag_sq += S_2f
             t += 1
-            np.append(spins, final_spin)
-            np.append(energies, E_f/n)
-            # it.append(t)
+
             continue
         elif P < bz:
+            energy += E_f
+            magnetisation += S_f
+            enrg_sq += E_2f
+            mag_sq += S_2f
             t += 1
-            np.append(spins, final_spin)
-            np.append(energies, E_f)
-            # it.append(t)
+
             continue
         else:
             # revert to the old state and move on
-            t += 1
             lattice[i][j] = old
-            np.append(spins, initial_spin)
-            np.append(energies, E_i)
-            # it.append(t)
+            energy += E_i
+            magnetisation += S_i
+            enrg_sq += E_2i
+            mag_sq += S_2i
+            t += 1
+
             continue
-    # Compute energy and magnetisation for this temperature
-    energy = np.divide(np.sum(energies), iterations)
-    spin = np.divide(np.sum(spins), iterations)
+
+    energy = energy/iterations
+    magnetisation = magnetisation/iterations
+    heat_cap = np.divide((enrg_sq - energy**2), (k_B*(T**2)))/iterations
+    suscept = np.divide((mag_sq - magnetisation**2), (k_B*T))/iterations
 
     # return the final state
-    # energy = np.divide(np.sum(energies), len(energies))
-    # spin = np.divide(np.sum(spins), len(spins))
-    return lattice, energy, spin
+    return lattice, energy, magnetisation, heat_cap, suscept
 
 # Make a plot of the spins
-def plot(lattice, i):
+def plot(lattice, i, T):
     cmap = colors.ListedColormap(['red', 'blue'])
 
     plt.figure(i)
-    plt.imshow(lattice)
-    plt.colorbar()
+    plt.imshow(lattice, cmap)
+
     # save the file by temperature identifier
-    name = "fig_" + str(50 + 10*i) + ".png"
+    name = "fig_" + str(i) + ".png"
+    plt.xlabel("T = " + str(T))
     plt.savefig(name)
     plt.close()
     return
@@ -158,28 +180,29 @@ N = 20
 iterations = 20000
 
 # Run for NxN lattice for temperatures between 0 and 300K in 10K intervals
-temperatures = np.arange(50, 310, 5)   
+temperatures = np.arange(1, 310, 1)   
 
 tick = time.time()
 
 energies = np.zeros((temperatures.size,))
 spins = np.zeros((temperatures.size,))
+heat_caps = np.zeros((temperatures.size,))
+susceptibilities = np.zeros((temperatures.size,))
 
 for i in range(0, temperatures.size):
-    T = str(temperatures[i]) + "K"
-    print("Temperature = " + T)
-    lattice, energy, spin = metropolis(initial_state(N,N), iterations, temperatures[i])
-    np.append(energies, energy)
-    np.append(spins, spin)
-    plot(lattice, i)
+    print("Temperature = " + str(temperatures[i]))
+    lattice, energy, spin, heat, suscept = metropolis(initial_state(N,N), iterations, temperatures[i])
+    energies[i] = energy
+    spins[i] = spin
+    heat_caps[i] = heat
+    susceptibilities[i] = suscept
 
-energies = np.array(energies)
-spins = np.array(spins)
+    #plot(lattice, i, T)
 
 plt.figure()
 plt.scatter(temperatures, energies)
 plt.xlabel("Temperature (K)")
-plt.ylabel("Lattice energy")
+plt.ylabel("Average energy E/J")
 plt.savefig("energy_vs_temperature.png")
 plt.close()
 
@@ -188,6 +211,18 @@ plt.scatter(temperatures, spins)
 plt.xlabel("Temperature (K)")
 plt.ylabel("Magnetisation")
 plt.savefig("magnetisation_vs_temperature.png")
+
+plt.figure()
+plt.scatter(temperatures, heat_caps)
+plt.xlabel("Temperature (K)")
+plt.ylabel("Heat capacity (Joules/KJ^2 )")
+plt.savefig("specific_heat_vs_temperature.png")
+
+plt.figure()
+plt.scatter(temperatures, susceptibilities)
+plt.xlabel("Temperature (K)")
+plt.ylabel("Susceptibility")
+plt.savefig("susceptibility_vs_temperature.png")
 
 # Measure time taken
 tock = time.time()
