3af0689bed911ccd266af6c5182ab6ab83a71cad
toor <william@huddie.net>
2022-04-21 12:33:59 +0100

    Add 3D model

--- ising.py
+++ ising.py
@@ -2,37 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 from metro import metropolis, initial_state
 import matplotlib.colors as clr
+from datetime import datetime
+import time
 
 # Begin adjustable parameters
-
-T_i = 1
-T_f = 305
-step = 1
+T_i = 300
+T_f = 340
+step = 0.1
 
 # I_eq: iterations required to reach the equilibrium state
-I_eq = 3000
+I_eq = 10000
 # I_s: iterations over which the thermodynamic properties
 # are calculated.
-I_s = 15000
+I_s = 100000
 # dimension of lattice. In this case we are using a square lattice,
 # though this code accounts for rectangular lattices also.
 n = 20
 
 # Make a plot of the spins
 def plot(lattice, T):
-    cmap = clr.ListedColormap(['red', 'blue'])
+    cmap = clr.ListedColormap(['black', 'white'])
 
     plt.figure()
     plt.imshow(lattice, cmap)
 
     # save the file by temperature identifier
-    name = "fig_" + str(T) + "K" + ".png"
+    name = "fig_" + str(T) + ".png"
     plt.xlabel("T = " + str(T))
     plt.savefig(name)
     plt.close()
     return
 
-def make_plots(data):
+def make_plots(data, T_c):
+    dt = datetime.fromtimestamp(time.time())
+    
     temperatures = data[0]
     energies = data[1]
     spins = data[2]
@@ -43,26 +46,26 @@ def make_plots(data):
     plt.plot(temperatures, energies, c="red")
     plt.xlabel("Temperature (K)")
     plt.ylabel("Average energy (J)")
-    plt.savefig("energy_vs_temperature.png")
+    plt.savefig("energy_vs_temperature_" + str(dt) + ".png")
 
     plt.figure()
-    plt.plot(temperatures, spins, c="red")
+    plt.scatter(temperatures, spins, c="red")
     plt.xlabel("Temperature (K)")
     plt.ylabel("Magnetisation")
-    plt.savefig("magnetisation_vs_temperature.png")
-
-    plt.figure()
-    plt.plot(temperatures, heat_caps, c="red")
-    plt.xlabel("Temperature (K)")
-    plt.ylabel("Heat capacity (JK^-1)")
-    plt.savefig("heat_capacity_vs_temperature.png")
+    plt.savefig("magnetisation_vs_temperature_" + str(dt) + ".png")
 
     plt.figure()
     plt.plot(temperatures, susceptibilities, c="red")
     plt.xlabel("Temperature (K)")
     plt.ylabel("Susceptibility")
-    plt.savefig("susceptibility_vs_temperature.png")
-    
+    plt.savefig("susceptibility_vs_temperature_" + str(dt) + ".png")
+
+    plt.figure()
+    plt.plot(temperatures, heat_caps, c="red")
+    plt.xlabel("Temperature (K)")
+    plt.ylabel("Heat capacity")
+    plt.savefig("heatcap_vs_temperature_" + str(dt) + ".png")
+ 
     return
 
 def ising():
@@ -74,6 +77,8 @@ def ising():
     heat_caps = np.zeros((temperatures.size,))
     susceptibilities = np.zeros((temperatures.size,))
     
+    temp_vals = np.arange(0, 400, 10)
+
     print("ising: Beginning calculation of thermodynamic properties.")
     for i in range(0, temperatures.size):
         T = temperatures[i]
@@ -83,28 +88,63 @@ def ising():
         # First solve for the equilibrium state. 
         eq_state = metropolis(initial_state(n,n), I_eq, T)[0]
         # Now calculate statistics 
-        data = metropolis(eq_state, I_s, T)[1]
+        data = metropolis(eq_state, I_s, T)[1] 
+
+        if T in temp_vals:
+            plot(eq_state, T)
 
         energies[i] = data[0]
         spins[i] = data[1]
         heat_caps[i] = data[2]
-        susceptibilities[i] = data[3]
-    
-    spins_i = spins[0:(temperatures.size - 1)]
-    spins_f = np.roll(spins, -1)[0:(temperatures.size - 1)]   
-    sz = spins_i.size
-    s_max = np.amax(spins)
-    s_min = np.min(spins)
-    
-    print("ising: Attempting to determine the critical temperature.")
-    for j in range(0, sz - 1):
-        slope1 = spins_f[j] - spins_i[j]
-        slope2 = spins_f[j + 1] - spins_i[j + 1]
-        if (slope2 - slope1) < 0 and np.abs((slope2 - slope1)) >= 3*(s_max - s_min)/4:
-            T_c = temperatures[j]
-            print("ising: The critical temperature is ~ " + str(T_c) + "K.")
+        susceptibilities[i] = data[3]    
 
-    data = np.array([temperatures, energies, spins, heat_caps, susceptibilities]) 
-    make_plots(data)
+
+    T_c = temperatures[phase_transition(susceptibilities)]
+    print("The phase transition occurs at a temperature of " + str(T_c) + " K")
+
+    data = np.array([temperatures, energies, spins, heat_caps, susceptibilities])
+    write_data(data)
+    make_plots(data, T_c)
+
+    return
+
+def write_data(data):
+    dt = datetime.fromtimestamp(time.time())
+    f = "ising_data_" + str(dt) + ".dat"
+
+    with open(f, 'w') as dat:
+        heading = "T    E    M    C    X\n"
+        dat.write(heading)
+        
+        for i in range(0, data[0].size):
+            line = str(data[0][i]) + "    " + str(data[1][i]) + "    " + str(data[2][i]) + "    " + str(data[3][i]) + "    " + str(data[4][i]) + "\n"
+            dat.write(line)
+        dat.close()
+
+
+def phase_transition(suscept_data):
+    suscept_i = suscept_data[0:(suscept_data.size - 1)]
+    # roll back each element by 1 index, so that
+    # we can subtract elements pairwise.
+    suscept_f = np.roll(suscept_data, -1)[0:(suscept_data.size - 1)]
+    gradients = np.zeros(suscept_i.shape)
+    
+    # Search for a sudden large increase in the susceptibility,
+    # corresponding to the phase transition
+    for i in range(0, suscept_i.size):
+        gradient = suscept_f[i] - suscept_i[i]
+        gradients[i] = gradient
+    j = np.where(gradients == np.amax(gradients))
+    return j
+
+
+def make_iteration_plots():
+    it = np.arange(0, 30000)
+
+    raw_energies = metropolis(initial_state(n,n), 30000, 10)[2]
+    plt.plot(it, raw_energies, c="purple")
+    plt.xlabel("Iterations")
+    plt.ylabel("Lattice energy")
+    plt.savefig("iterations_vs_temperature_n=" + str(n))
 
 ising()
--- metro3d.py
+++ metro3d.py
@@ -12,7 +12,7 @@ J = np.power(10.0, -21)
 
 # Run metropolis on a specific lattice for a given temperature
 def metropolis(lattice, iterations, T):
-    (x, y) = lattice.shape
+    (x, y, z) = lattice.shape
 
     energies = np.zeros((iterations,))
     spins = np.zeros((iterations,))
@@ -26,15 +26,16 @@ def metropolis(lattice, iterations, T):
         # uniformally distributed
         i = np.random.randint(x)
         j = np.random.randint(y)
-       
+        k = np.random.randint(z)
+
         # calculate the total energy in units of J, and thus
         # the average energy per spin 
-        old = lattice[i][j]
+        old = lattice[i][j][k]
         E_i, E_2i, S_i, S_2i, E_i_total = energy_and_spin(lattice)
 
         # update to the new state
         new = -1*old
-        lattice[i][j] = new
+        lattice[i][j][k] = new
     
         E_f, E_2f, S_f, S_2f, E_f_total = energy_and_spin(lattice)
 
@@ -62,7 +63,7 @@ def metropolis(lattice, iterations, T):
             continue
         else:
             # revert to the old state and move on
-            lattice[i][j] = old
+            lattice[i][j][k] = old
             energies[t] = E_i
             spins[t] = S_i
             energies_sq[t] = E_2i
@@ -70,14 +71,14 @@ def metropolis(lattice, iterations, T):
             t += 1
 
             continue
-
+    
     energy = np.mean(energies)
     magnetisation = np.mean(spins)
     heat_cap = np.var(energies)
     suscept = np.var(spins)
 
     # return the final state and the associated thermodynamic statistics
-    return lattice, np.array([energy, magnetisation, heat_cap, suscept]), energies
+    return lattice, np.array([energy, magnetisation, heat_cap, suscept])
 
 # All these below functions exploit numba's speedup capability
 # for code which relies on a lot of loops and iterations
@@ -85,34 +86,36 @@ def metropolis(lattice, iterations, T):
 # Create an initial lattice with the spins in a pseudo-random
 # configuration
 @jit(nopython=True)
-def initial_state(x, y):
+def initial_state(x, y, z):
     # 75% negative
     p = 0.25
     # return array of random floats between 0,1 with the same shape as
     # the lattice
-    rand = np.random.random((x,y))
-    lattice = np.zeros((x,y))
+    rand = np.random.random((x,y,z))
+    lattice = np.zeros((x,y,z))
     for i in range(0, x):
         for j in range(0,y):
-            # p% of the spins up
-            if rand[i][j] >= p:
-                lattice[i][j] = 1
-            # (1 - p)% of the spins down
-            else:
-                lattice[i][j] = -1
-    
+            for k in range(0,z):
+                # p% of the spins up
+                if rand[i][j][k] >= p:
+                    lattice[i][j][k] = 1
+                # (1 - p)% of the spins down
+                else:
+                    lattice[i][j][k] = -1
     return lattice
 
 
 # Determine the neighbours of a certain lattice site (i,j)
 @jit(nopython=True)
-def nearest_neighbours(lattice, i, j):
-    (x,y) = lattice.shape
+def nearest_neighbours(lattice, i, j, k):
+    (x,y,z) = lattice.shape
     
     # Exploit modulo operator to calculate the nearest neighbours
     # -1 -> lattice_dim1 - numpy interprets an index -1 as taking us to the other 
     # side of the array
-    nn = [lattice[(i - 1)%x][j], lattice[(i+1)%x][j], lattice[i][(j-1)%y], lattice[i][(j + 1)%y]]
+    nn = [lattice[(i - 1)%x][j][k], lattice[(i+1)%x][j][k],
+            lattice[i][(j-1)%y][k], lattice[i][(j + 1)%y][k],
+            lattice[i][j][(k-1)%z], lattice[i][j][(k+1)%z]]
 
     nn = np.array(nn)
 
@@ -124,16 +127,17 @@ def nearest_neighbours(lattice, i, j):
 def calculate_total_energy(lattice):
     energy = 0
     # rows, cols
-    (x, y) = lattice.shape
+    (x, y, z) = lattice.shape
     # iterate over rows
     for i in range(0, x):
         # iterate over columns
         for j in range(0, y):
-            spin = lattice[i][j]
-            nn = nearest_neighbours(lattice, i, j)
-            for neighbour in nn:
-                contribution = -J*spin*neighbour
-                energy += contribution
+            for k in range(0,z):
+                spin = lattice[i][j][k]
+                nn = nearest_neighbours(lattice, i, j, k)
+                for neighbour in nn:
+                    contribution = -J*spin*neighbour
+                    energy += contribution
    
     return energy
 
@@ -142,16 +146,17 @@ def calculate_total_energy(lattice):
 @jit(nopython=True)
 def calculate_total_spin(lattice):
     spin = 0
-    (x,y) = lattice.shape
+    (x,y,z) = lattice.shape
     for i in range(0,x):
         for j in range(0,y):
-            _spin = lattice[i][j]
-            spin += _spin
+            for k in range(0,z):
+                _spin = lattice[i][j][k]
+                spin += _spin
     return spin
 
-@jit(nopython=True)
 # Returns the total average energy per spin, average spin,
 # average energy squared and average spin squared
+@jit(nopython=True)
 def energy_and_spin(lattice):
     n = lattice.size
     E_total = calculate_total_energy(lattice)
